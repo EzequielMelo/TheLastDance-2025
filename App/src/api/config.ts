ae740a81462a6e6e3d3617273d6e067c66d649d6
EzequielMelo <ezequielanthonymelo@gmail.com>
2025-10-07 21:22:07 -0300

    Ocultar archivos de configuración privada

--- App/src/api/config.ts
+++
@@ -1,9 +0,0 @@
-// Para dispositivo físico - usa la IP de tu computadora
-export const API_BASE_URL = "https://restaurant-1ocs.onrender.com/api"; // IP local encontrada
-
-// Para emulador Android:
-// export const API_BASE_URL = "http://10.0.2.2:3000/api";
-
-// Para producción:
-// export const API_BASE_URL = "https://tu-server.com/api";
-//http://10.0.2.2:3000/api Conexión emulador android
